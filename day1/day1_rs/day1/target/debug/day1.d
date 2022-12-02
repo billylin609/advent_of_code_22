e65196ea8a63277f4b7c5e099871e3da340639e2
billylin609 <linyuchen830@gmail.com>
2022-12-02 18:25:08 -0500

    day1 update

---
+++ day1/day1_rs/day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+C:\Users\linyu\Advent_of_code_22\day1_rs\day1\target\debug\day1.exe: C:\Users\linyu\Advent_of_code_22\day1_rs\day1\src\main.rs
---
+++ day1/day1_rs/day1/target/debug/deps/day1.d
@@ -0,0 +1,5 @@
+C:\Users\linyu\Advent_of_code_22\day1_rs\day1\target\debug\deps\day1.exe: src\main.rs
+
+C:\Users\linyu\Advent_of_code_22\day1_rs\day1\target\debug\deps\day1.d: src\main.rs
+
+src\main.rs:
